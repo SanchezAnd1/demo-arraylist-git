36a802b00ff6897b8b91fb62c7035f1e6876fbdc
Alex <SanchezAnd3@gmail.com>
2024-12-23 20:55:08 +0300

    City

---
+++ src/main/java/by/itacademy/alexlinnik/City.java
@@ -0,0 +1,15 @@
+package by.itacademy.alexlinnik;
+
+import java.util.ArrayList;
+
+public class City {
+    public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Moscow");
+        city.add("Minsk");
+        city.add("Brest");
+        city.add("New York");
+        city.add("Grodno");
+        System.out.println(city);
+    }
+}
